c67acb6e8a9e4d12c1b3b02e3a27276fff1a2506
Crabu <88827804+Craby13@users.noreply.github.com>
2022-11-20 18:39:11 +0100

    Update code_cesar.py

--- code_cesar.py
+++ code_cesar.py
@@ -1,4 +1,3 @@
-print("Bonjour, que voulez-vous faire ?")
 list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 for x in range(len(list)):
